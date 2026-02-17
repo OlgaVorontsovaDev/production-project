467f12e1aa63ed3eece3016a78fa3afda6b9ddc8
Olga Vorontsova <olga.vorontsova.dev@gmail.com>
2026-02-17 16:53:54 +0300

    set up css modules

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,6 @@
-import path from 'path';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 import { BuildOptions } from './types/config';
 
@@ -12,5 +12,9 @@ export function buildPlugins({
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].contenthash:8].css',
+    }),
   ];
 }
