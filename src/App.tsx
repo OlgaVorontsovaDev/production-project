6caea5c01a9f7336454875fa6b3cff615083c959
Olga Vorontsova <olga.vorontsova.dev@gmail.com>
2026-02-18 17:23:29 +0300

    organized styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import Counter from './components/Counter';
-import './index.scss';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Suspense, useContext } from 'react';
+
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+
+import { useTheme } from './theme/useTheme';
+
+import './styles/index.scss';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className='app'>
-      frefevgtrbEFERGfw
-      <Counter />
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE THEME</button>
+      <Link to='/'>Главная</Link>
+      <Link to='/about'>О сайте</Link>
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Routes>
+          <Route path='/about' element={<AboutPageAsync />} />
+          <Route path='/' element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
